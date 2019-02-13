eae48fcddf1d522687dd356ab7e1ebde03602105
Kingaa6 <Kingaa6@gmail.com>
2019-02-13 13:14:37 -0800

    Add post method to posts route

--- Posts/post-router.js
+++ Posts/post-router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const db = require('./postDb');
+const usersDb = require('../Users/user-router');
 
 // incoming '/posts' from the server
 
@@ -41,7 +42,58 @@ router.get('/:id', (req, res) => {
     );
 });
 
-router.post('/', (req, res) => {});
+// get posts by user
+router.get('/user/:id', (req, res) => {
+  const user_id = req.params.id;
+  console.log(user_id);
+
+  usersDb
+    .getUserPosts(user_id)
+    .then(posts => {
+      if (!posts) {
+        res.status(404).json({
+          success: false,
+          message:
+            'The post you are looking for does not exist, please try another id'
+        });
+      } else {
+        res.status(200).json({ success: true, posts });
+      }
+    })
+    .catch(err =>
+      res.status(500).json({
+        success: false,
+        message: 'An error occurred while retrieving the posts'
+      })
+    );
+});
+
+// create a new post, requires text field and user id
+router.post('/', (req, res) => {
+  const { text, user_id } = req.body;
+  const newPost = req.body;
+
+  console.log(text);
+  console.log(newPost);
+
+  if (!text || !user_id) {
+    res.status(400).json({
+      success: false,
+      message: 'Unable to create a new post, missing a required input'
+    });
+  }
+
+  db.insert(newPost)
+    .then(post => {
+      res.status(201).json({ success: true, post });
+    })
+    .catch(err =>
+      res.status(500).json({
+        success: false,
+        message: 'An error occurred while creating the post'
+      })
+    );
+});
 
 router.delete('/', (req, res) => {});
 
