27bb8cdd38d3c682f1497eb6f0187d3c2050ab4c
Kingaa6 <Kingaa6@gmail.com>
2019-02-13 12:23:20 -0800

    Add basic server structure

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code away!
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use(morgan('dev'));
+
+server.get('/', (req, res) => {
+  res.status(200).json({ success: true });
+});
+
+server.listen(5000, () => {
+  console.log('* Server Running on Port 5000 *');
+});
